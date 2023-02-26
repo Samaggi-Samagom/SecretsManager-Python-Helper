9a11ee4e245b0dcc6867b591c488b384a8a727f8
bookla <booklailert@gmail.com>
2023-02-26 11:51:31 +0000

    HashGenerator now a class of Security

---
+++ SecretsManagerInterface/Security.py
@@ -0,0 +1,15 @@
+import hashlib
+
+
+class HashGenerator:
+
+    def __init__(self):
+        self.__salt = "samaggisamagom"
+
+    def set_salt(self, salt: str):
+        self.__salt = salt
+
+    def get(self, value: str, salt: str = None):
+        if salt is None:
+            salt = self.__salt
+        return hashlib.sha256((hashlib.sha256(value.encode("utf-8")).hexdigest() + salt).encode("utf-8")).hexdigest()
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
    name='SecretsManagerInterface',
-   version='1.0.3',
+   version='1.0.4',
    description='Python Interface to Streamline Access to SecretsManager secrets.',
    long_description=long_description,
    author='Pakkapol Lailert',
